936cbcaf6a1f36e21cc49e456dbf0dc41c63f584
Luciano Ferreyro <lucianopabloferreyro@gmail.com>
2022-04-22 16:17:17 -0300

    Ejemplo y TP1

---
+++ suma_abi/src/suma_abi.s
@@ -0,0 +1,23 @@
+.extern suma_c
+.extern _PUBLIC_STACK_INIT
+
+.global _start
+
+.code 32
+.section .text
+_start:
+    LDR SP,=_PUBLIC_STACK_INIT
+
+    MOV R0, #0x10
+    MOV R1, #0x20
+
+    /* B suma_c */
+
+    LDR R10, =suma_c
+    MOV LR, PC
+    BX R10
+
+    B .
+    
+.end
+
